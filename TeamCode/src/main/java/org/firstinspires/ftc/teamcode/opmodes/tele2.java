e4d8058c417de945a97c537a883c5d3be4c9a650
Ekansh <ekagupta2001@gmail.com>
2026-02-25 07:31:14 -0700

    telemetry Data Added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tele2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tele2.java
@@ -101,6 +101,7 @@ public class tele2 extends OpMode {
 
         telemetry.addData("X", robot.follower.getPose().getX());
         telemetry.addData("Y", robot.follower.getPose().getY());
+        telemetry.addData("Current Pose", PoseStorage.currentPose);
         telemetry.addData("Last detected distance: ", robot.colorSensor.lastDistance);
         telemetry.addData("Calibrated: ", calibrated);
         telemetry.addData("Shooter Velocity", robot.shooter.getVelocity());
